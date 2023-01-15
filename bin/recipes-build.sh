a4ffe24dcebde728dbe8d6a036356cac7f345410
mitchelbade <mitchelm.bade@gmail.com>
2023-01-14 22:55:43 -0800

    recipe app again

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 rm -rf public
 npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public
+cp -a client/build/. public/
 
 bundle install
 bundle exec rake db:migrate 
